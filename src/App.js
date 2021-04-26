e6cccd1786ed46bf4cfb0a107542e87b071ea636
Valentin Manes <valentinmanes@outlook.fr>
2021-04-26 17:42:07 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Container, Row, Col} from 'reactstrap';
+
+//import {ResponsiveStream} from '@nivo/stream';
+
 import './App.css';
 
-function App() {
+const AppCont = ({ children }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section>
+      <Container className="my-5 py-5">
+        <Row className="text-center"><Col>
+          {children}
+        </Col></Row>
+      </Container>
+    </section>
+  )
+}
+
+const RangeSlider = ({ rangeval, setRangeval }) => {
+  return (
+    <div>
+      <input type="range" min="0" max="100" className="slider" defaultValue={rangeval}
+        onChange={(event) => {
+          return setRangeval(event.target.value);
+        }} />
+      <h4>The range value is {rangeval}</h4>
     </div>
   );
+};
+
+const App = () => {
+  const [rangeval, setRangeval] = useState(10);
+  const randomData = Array.from({ length: 8 }, (_, i) => {
+    return {
+    "Raoul": i+1,
+    "Jacques": 2 * (1+i)
+    };
+  })
+
+  return (
+    <>
+      <AppCont >
+        <h1>Hello sira</h1>
+        <h2>Slider value: {rangeval}</h2>
+        <RangeSlider rangeval={rangeval} setRangeval={setRangeval} />
+      </AppCont>
+      <AppCont >
+        {/*<div style={{ height: 400 }}>
+          <MyResponsiveStream data={randomData} />
+        </div>*/}
+      </AppCont>
+    </>
+  );
 }
+
+//const MyResponsiveStream = ({ data }) => (
+//  <ResponsiveStream
+//      data={data}
+//      keys={[ 'Raoul', 'Jacques' ]}
+//      enableGridX={false}
+//      enableGridY={true}
+//      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+//      axisTop={null}
+//      axisRight={null}
+//      axisBottom={{
+//          orient: 'bottom',
+//          tickSize: 5,
+//          tickPadding: 5,
+//          tickRotation: 0,
+//          legend: '',
+//          legendOffset: 36
+//      }}
+//      axisLeft={{ orient: 'left', tickSize: 5, tickPadding: 5, tickRotation: 0, legend: '', legendOffset: -40 }}
+//      offsetType="diverging"
+//      colors={{ scheme: 'blues' }}
+//      fillOpacity={0.85}
+//      borderColor={{ theme: 'background' }}
+//      legends={[
+//          {
+//              anchor: 'bottom-right',
+//              direction: 'column',
+//              translateX: 100,
+//              itemWidth: 80,
+//              itemHeight: 20,
+//              itemTextColor: '#999999',
+//              symbolSize: 12,
+//              symbolShape: 'circle',
+//              effects: [
+//                  {
+//                      on: 'hover',
+//                      style: {
+//                          itemTextColor: '#000000'
+//                      }
+//                  }
+//              ]
+//          }
+//      ]}
+//  />
+//)
 
 export default App;
