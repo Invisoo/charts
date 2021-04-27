79c4e34f5e276cbfa23eb4e780ecd72bc99eb780
Valentin Manes <valentinmanes@outlook.fr>
2021-04-27 12:42:20 +0200

    WIP: Testing lines instead of stream

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {Container, Row, Col} from 'reactstrap';
 
 import {ResponsiveStream} from '@nivo/stream';
+import {ResponsiveLine} from '@nivo/line';
 
 import './App.css';
 
@@ -121,6 +122,74 @@ const MyResponsiveStream = ({ data }) => (
                       on: 'hover',
                       style: {
                           itemTextColor: '#000000'
+                      }
+                  }
+              ]
+          }
+      ]}
+  />
+)
+
+const MyResponsiveLine = ({ data }) => (
+  <ResponsiveLine
+      data={data}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      xScale={{ type: 'point' }}
+      yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+      yFormat=" >-.2f"
+      curve="catmullRom"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+          orient: 'bottom',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'transportation',
+          legendOffset: 36,
+          legendPosition: 'middle'
+      }}
+      axisLeft={{
+          orient: 'left',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'count',
+          legendOffset: -40,
+          legendPosition: 'middle'
+      }}
+      enableGridX={false}
+      lineWidth={6}
+      enablePoints={false}
+      pointSize={10}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabelYOffset={-12}
+      enableArea={true}
+      areaOpacity={0.1}
+      enableSlices="x"
+      legends={[
+          {
+              anchor: 'top',
+              direction: 'column',
+              justify: false,
+              translateX: 0,
+              translateY: -48,
+              itemsSpacing: 0,
+              itemDirection: 'left-to-right',
+              itemWidth: 107,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 14,
+              symbolShape: 'circle',
+              symbolBorderColor: 'rgba(0, 0, 0, .5)',
+              effects: [
+                  {
+                      on: 'hover',
+                      style: {
+                          itemBackground: 'rgba(0, 0, 0, .03)',
+                          itemOpacity: 1
                       }
                   }
               ]
