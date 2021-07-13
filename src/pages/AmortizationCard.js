3b2bceaa82773a124339c32af9c894d938383276
Valentin Manes <valentinmanes@outlook.fr>
2021-07-13 19:13:49 +0200

    Typo

--- src/pages/AmortizationCard.js
+++ src/pages/AmortizationCard.js
@@ -26,7 +26,7 @@ const AmortizationCard = () => {
 	return (
 		<Card className="my-2"><CardBody className="text-center">
 			<p>
-				Je vais depensez
+				Je vais depenser
 				$<input
 					style={{ width: "75px" }}
 					type="number" min="0" step="1"
